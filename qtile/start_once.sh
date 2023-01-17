9f617336e71527df7fbb019b279751c8c61a539d
davvied <s.david.m.z@gmail.com>
2023-01-17 19:02:44 +0330

    added polkit helper

--- qtile/start_once.sh
+++ qtile/start_once.sh
@@ -4,5 +4,6 @@
 $HOME/.screenlayout/res.sh &
 # nitrogen --restore &
 nm-applet &
-pasystray &
+# pasystray &
+/usr/lib/polkit-1/polkit-agent-helper-1 &
 picom --vsync --fade-in-step=0.4 --fade-out-step=0.4 --shadow --blur-background --daemon &
