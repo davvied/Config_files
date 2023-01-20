86c67d59b5ea6ab65500689f86ff3e5e9b629263
davvied <s.david.m.z@gmail.com>
2023-01-20 16:55:12 +0330

    changed scripts from bash to sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,5 +4,6 @@
 # xargs xwallpaper --stretch < ~/.cache/wall &
 # 2. Uncomment to set a random wallpaper on login
 # find /usr/share/backgrounds/elementary_wallpapers -type f | shuf -n 1 | xargs xwallpaper --stretch &
-find $HOME/Pictures/wallpaper -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# find $HOME/Pictures/wallpaper -type f | shuf -n 1 | xargs xwallpaper --stretch &
 # 3. Uncomment to set wallpaper with nitrogen
+xwallpaper --output LVDS1 --stretch (find /home/fuji/wallpapers/waves | shuf -n 1)
--- qtile/start_once.sh
+++ qtile/start_once.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/fish
 /usr/bin/emacs --daemon &
-
+eval (ssh-agent -c) &
 $HOME/.screenlayout/res.sh &
 # nitrogen --restore &
 nm-applet &
 firewall-applet &
 # pasystray &
 polkit-dumb-agent &
-picom --vsync --fade-in-step=0.4 --fade-out-step=0.4 --shadow --blur-background --daemon &
+picom --vsync --fade-in-step=0.4 --fade-out-step=0.4 --shadow --blur-background &
